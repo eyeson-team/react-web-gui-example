44fbe001aba91788e095aaac5685becafcafd384
Stefan Benicke <stefan.benicke@gmail.com>
2021-11-03 10:40:07 +0100

    remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,14 @@ import ReactDOM from 'react-dom';
 import 'rmwc/dist/styles';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+// remove discontinued service worker installations
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then((registration) => {
+        registration.unregister();
+    }).catch((error) => {
+        console.error(error.message);
+    });
+}
